ed13701145e07922787b486c142ea03a475f5319
alison <alison@metanoia>
2019-01-16 10:51:39 -0300

    Desacoplada configuração SECRET_KEY
    close #22

--- partyou/settings.py
+++ partyou/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gd4+dv-rd^cj%(!jcd0ni7-9=bccfr%)low1%#c&%nm($z7mk8'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
