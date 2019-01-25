faadddbb3a9a29639616d3f30b80682c8b91ebe9
Tiago Cordeiro <tiago@mulhergorila.com>
2019-01-25 17:13:42 -0200

    Testes simples renderizando Produtos e Categorias na Home Page

--- partyou/base/views.py
+++ partyou/base/views.py
@@ -2,12 +2,15 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 
 from partyou.base.forms import RegisterForm
-from partyou.produtos.models import Category
+from partyou.produtos.models import Category, Product
 
 
 def home(request):
     categories = Category.objects.all()
-    return render(request, 'base/home.html', {'categories': categories})
+    produtos = Product.objects.all()
+    context = {'categories': categories,
+               'produtos': produtos,}
+    return render(request, 'base/home.html', context)
 
 
 def about(request):
