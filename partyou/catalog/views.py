f2b7317831db3e1fe2b210db7f53aa4425bcbf69
alison <alison@metanoia>
2019-01-29 18:33:21 -0300

    Criadas Class Based Views
    close #119

--- partyou/catalog/views.py
+++ partyou/catalog/views.py
@@ -1,22 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product, Category
+from django.views import generic
 
 
-def product_list(request):
-    context = {
-        'product_list': Product.objects.all()
-    }
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(generic.ListView):
+
+    queryset = Product.objects.all()
+    # model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 3
 
 
-def category(request, slug):
-    category = Category.objects.get(slug=slug)
-    context = {
-        'current_category': category,
-        'product_list': Product.objects.filter(category=category),
-    }
-    return render(request, 'catalog/category.html', context)
+product_list = ProductListView.as_view()
+
+
+class CategoryListView(generic.ListView):
+
+    template_name = 'catalog/category.html'
+    context_object_name = 'product_list'
+    paginate_by = 3
+
+    def get_queryset(self):
+        return Product.objects.filter(category__slug=self.kwargs['slug'])  # pragma: no cover
+
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)  # pragma: no cover
+        context['current_category'] = get_object_or_404(Category, slug=self.kwargs['slug'])  # pragma: no cover
+        return context  # pragma: no cover
+
+
+category = CategoryListView.as_view()
 
 
 def product(request, slug):
