8432589d127e6095ff9d446238bf380462b357ef
alison <alison@metanoia>
2019-01-30 17:46:35 -0300

    Criada aplicação Checkout
    close #127

---
+++ partyou/checkout/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from partyou.catalog.models import Product
+
+
+class CartItem(models.Model):
+
+    cart_key = models.CharField(
+        'Chave do Carrinho', max_length=40, db_index=True
+    )
+    product = models.ForeignKey(Product, verbose_name='Produto', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField('Quantidade', default=1)
+    price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
+
+    class Meta:
+        verbose_name = 'Item do Carrinho'
+        verbose_name_plural = 'Itens dos Carrinhos'
+
+    def __str__(self):
+        return '{} [{}]'.format(self.product, self.quantity)
