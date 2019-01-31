642daf38911de2509c73525556ffafa32aea63a7
alison <alison@metanoia>
2019-01-31 20:47:34 -0300

    Criada página de carrinho de compras
    close #134

--- partyou/catalog/admin.py
+++ partyou/catalog/admin.py
@@ -3,17 +3,19 @@ from django.contrib import admin
 from partyou.catalog.models import Product, Category
 
 
-@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-
     list_display = ['name', 'slug', 'category', 'created', 'modified']
     search_fields = ['name', 'slug', 'category__name']
     list_filter = ['created', 'modified']
+    prepopulated_fields = {"slug": ('name',)}
 
 
-@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-
     list_display = ['name', 'slug', 'created', 'modified']
     search_fields = ['name', 'slug']
     list_filter = ['created', 'modified']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
